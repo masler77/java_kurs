f457b6be37b8b3882964332df70e419dad8e0716
Nolsoy <N.Eldstahl@hotmail.com>
2015-10-20 11:00:49 +0200

    Update Kurs.java
    Test

--- kurs/src/kurs/Kurs.java
+++ kurs/src/kurs/Kurs.java
@@ -16,6 +16,8 @@ public class Kurs {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Mange is the man!");
     }
     
 }
